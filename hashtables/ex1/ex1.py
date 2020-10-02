c7973eda3fe0efc8fabfca21fcbfe5e413f01f52
Jessica <duellje@oregonstate.edu>
2020-10-02 11:10:10 -0400

    working on ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,26 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    table = {}
+    
+    for index in range(length):
+        table[weights[index]] = index
+        
+    print(table)
 
-    return None
+    if limit in table:
+        return table[limit]
+    else:
+        sums = []
+        for k in table:
+            if limit - k in table:
+                sums.append(table[k])
+        return sums 
+
+if __name__ == "__main__":
+    # weights = [ 4, 6, 10, 15, 16 ]
+    # length = 5
+    # limit = 21
+    # #[3, 1] --> indices of weights whose sum = 21
+    # print(get_indices_of_item_weights(weights, length, limit))
+    
+    weights_2 = [4, 4]
+    print(get_indices_of_item_weights(weights_2, 2, 8))
