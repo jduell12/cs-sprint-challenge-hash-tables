3b5be2611b99782d872be2cc80a8934cd6f8022a
Jessica <duellje@oregonstate.edu>
2020-10-02 11:34:33 -0400

    working on ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,37 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
+        
+    def __repr__(self):
+        return f"Ticket({self.source}, {self.destination})"
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    itinerary = []
+    middle = {}
+    
+    for ticket in tickets:
+        if ticket.source == "NONE":
+            itinerary.insert(0, ticket.destination)
+        elif ticket.destination == "NONE":
+            itinerary.insert(length-1, ticket.source) 
+        else:
+            pass
+    return itinerary
 
-    return route
+tickets = [
+    Ticket("PIT",  "ORD" ),
+    Ticket("XNA", "CID" ),
+    Ticket( "SFO",  "BHM" ),
+    Ticket( "FLG",  "XNA" ),
+    Ticket("NONE",  "LAX" ),
+    Ticket ("LAX",  "SFO" ),
+    Ticket ("CID",  "SLC" ),
+    Ticket ("ORD",  "NONE" ),
+    Ticket("SLC",  "PIT" ),
+    Ticket("BHM",  "FLG" )
+]
+print(reconstruct_trip(tickets, len(tickets)))
+
+# expected = ["PDX", "DCA", "NONE"]
