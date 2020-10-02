1b60121deddd765c604b80e86c496440018df388
Jessica <duellje@oregonstate.edu>
2020-10-02 12:35:34 -0400

    working ex5
    ;

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,26 @@
-# Your code here
-
-
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    hash_tbl = {}
+        
+    for file in files:
+        # parts = file.split('/')[-1]
+        parts = file.split('/')
+        #remove empty space
+        parts.pop(0)
+        
+        for part in parts:
+            if part in hash_tbl:
+                hash_tbl[part].append(file)
+            else:
+                hash_tbl[part] = [file]
+    
+    file_queries = []
+    
+    for query in queries:
+        if query in hash_tbl:
+            file_queries += hash_tbl[query]
 
-    return result
+    return file_queries
 
 
 if __name__ == "__main__":
