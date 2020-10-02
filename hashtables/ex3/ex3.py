500c2e7830d67901a7503b8dd872eef38ceec7a7
Jessica <duellje@oregonstate.edu>
2020-10-02 11:58:46 -0400

    working ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -6,22 +6,27 @@ def intersection(arrays):
             if num not in freq:
                 freq[num] = 0
             freq[num] += 1
+    
+    freq_list = list(freq.items())
+    freq_list.sort(key=lambda t:t[1], reverse=True)
+    
+    iter = []
+    
+    for i in range(len(freq_list)):
+        if freq_list[i][1] == len(arrays):
+            iter.append(freq_list[i][0])
+        else:
+            break
+    
+    return iter
 
-    return freq
 
 
 if __name__ == "__main__":
-    # arrays = []
+    arrays = []
 
-    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    # print(intersection(arrays))
-
-    arrays = [
-        [1, 2, 3, 4, 5],
-        [12, 7, 2, 9, 1],
-        [99, 2, 7, 1,]
-    ]
-    print(intersection(arrays)) #[1,2]
+    print(intersection(arrays))
